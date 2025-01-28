f3a18409b5d037fbf157a7a73dde07026ba16d1e
Matthew Wells <matthewpaulwells@gmail.com>
2025-01-28 09:07:53 -0500

    snacks now move

--- src/snack.lua
+++ src/snack.lua
@@ -3,6 +3,7 @@ import "CoreLibs/sprites"
 
 local tagActive <const> = 1
 local tagRemoved <const> = 2
+local rand <const> = math.random
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
@@ -12,10 +13,22 @@ class("Snack").extends(gfx.sprite)
 --- @class Snack: _Sprite
 Snack = Snack or {}
 
+Snack.xDirection = 0
+Snack.yDirection = 0
+
+function getMovement()
+   local direction = rand(1, 3)
+   if rand(1, 2) == 1 then
+      return -1 * direction
+   end
+   return direction
+end
+
 function Snack:init()
    Snack.super.init(self)
    self:setImage(assert(gfx.image.new("images/snack")))
-   self:moveTo(math.random(400), math.random(240))
+   self:moveTo(rand(400), rand(240))
+   self.xDirection, self.yDirection = getMovement(), getMovement()
    self:setCollideRect(0, 0, self:getSize())
 end
 
@@ -27,6 +40,17 @@ end
 function Snack:remove()
    Snack.super.remove(self)
    self:setTag(tagRemoved)
+end
+
+function Snack:update()
+   local posX, posY = self:getPosition()
+   if posX <= 0 or posX >= 400 then
+      self.xDirection = self.xDirection * -1
+   end
+   if posY <= 0 or posY >= 240 then
+      self.yDirection = self.yDirection * -1
+   end
+   self:moveBy(self.xDirection, self.yDirection)
 end
 
 --- Make a number of snack sprites
